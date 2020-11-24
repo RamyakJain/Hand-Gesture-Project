4cbdf15bf8c0e0fa699ad78a261ee6ad44df1b87
RamyakJain <73877400+RamyakJain@users.noreply.github.com>
2020-11-24 18:48:37 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -16,3 +16,36 @@ classifier = ("https://teachablemachine.withgoogle.com/models/pxoEwzE6U/model.js
 function modelLoaded(){
     console.log("Model Loaded!");
 }
+function check(){
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+function gotResult(error, results){
+    if (error){
+        console.error(error);
+    }
+    else{
+       connsole.log(results); 
+       document.getElementById("result_gesture_name").innerHTML = results[0].label;
+    }
+    if (results[0].label == "amazing"){
+        document.getElementById("update_gesture").innerHTML = "&#128076;";
+    }
+    if (results[0].label == "nice"){
+        document.getElementById("update_gesture").innerHTML = "&#128077;";
+    }
+    if (results[0].label == "victory"){
+        document.getElementById("update_gesture").innerHTML = "&#9996;";
+    }
+    if (results[0].label == "bad"){
+        document.getElementById("update_gesture").innerHTML = "&#128078;";
+    }
+    if (results[0].label == "stop"){
+        document.getElementById("update_gesture").innerHTML = "&#128400;";
+    }
+    function speak(){
+        var synth = window.speechSynthesis;
+        speak_data = results[0].label;
+        synth.speak(utterThis);
+    }
+}
